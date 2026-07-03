8c4893af9f0e7b29df0b979d52c39517c7833e3e
aarav-tech-123 <developer@aaravtech.net>
2026-07-03 17:02:55 -0400

    Update db.js

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   options: {
-    encrypt: false,
+    encrypt: true,
     trustServerCertificate: true,
     enableArithAbort: true,
   }
